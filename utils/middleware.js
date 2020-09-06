8765c656ab736dcd8434c5b6de4baf9eeed2305d
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-09-06 14:37:05 +0800

    Improved errorHandler middleware to support database validation error

--- utils/middleware.js
+++ utils/middleware.js
@@ -10,6 +10,15 @@ const unknownEndPoint = (req, res) => {
 };
 
 const errorHandler = (error, req, res, next) => {
+  if (error.name === "ValidationError") {
+    console.log(error.properties);
+    let errorObject = {};
+    Object.values(error.errors).forEach(
+      ({ properties }) => (errorObject[properties.path] = properties.message)
+    );
+    return res.status(400).json({ error: errorObject });
+  }
+
   logger.error(error.message);
   next(error);
 };
