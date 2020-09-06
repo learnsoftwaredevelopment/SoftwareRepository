cb2da0567c7f6a5dd7f65566920f7cb7bef12ed1
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-09-06 14:39:03 +0800

    Added users controller

---
+++ controllers/users.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+const config = require("../utils/config");
+
+usersRouter.get("/", async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+usersRouter.post("/", async (req, res) => {
+  const body = req.body;
+
+  if (!body.password) {
+    res.status.json({
+      error: "`password` is required.",
+    });
+  } else if (body.password.length < 8) {
+    res.status.json({
+      error: "Password has to be at least 8 characters long.",
+    });
+  }
+
+  const saltRounds = config.BCRYPT_SALT_ROUNDS;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    email: body.email,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  res.json(savedUser);
+});
+
+module.exports = usersRouter;
