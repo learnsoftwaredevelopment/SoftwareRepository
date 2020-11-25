eb6a56469ef5c2f29241d6dded6a341485412431
LEE WEI HAO JONATHAN <leeweihao1996@outlook.com>
2020-11-25 20:17:54 +0800

    Improved GitHub Actions ci to use the new env variables
    Amended `dockerfile`, `dockerfile-ci,` `docker-compose.yml` with the new firebase admin service account environment variables
    Removed the encrypted secret `firebase.gpg`, the job for decryption of encrypted secrets, the `decrypt_firebase_secret.sh` shell script
    Removed `GOOGLE_APPLICATION_CREDENTIALS` environment variable which is no longer used

--- .github/scripts/decrypt_firebase_secret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Script to decrypt firebase secret
-
-# Create the environment and decrypt the file
-mkdir $HOME/secrets
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPTION_PASSPHRASE" --output firebase_secret.json ./.github/ci/firebase.gpg
